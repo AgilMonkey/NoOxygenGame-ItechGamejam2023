3867e7a7214ac3007ef77b8465f607343e4972da
AgilMonkey <muhammadagilfadilah@gmail.com>
2023-09-15 18:52:08 +0800

    Menghilangkan objek jika jauh dan membuat ui game over

---
+++ scenes/ui/game-ui/game_ui.gd
@@ -0,0 +1,11 @@
+extends Control
+
+@onready var health_ui : Label = $HealthUI
+@onready var oxygen_ui : Label = $OxygenUI
+
+func _on_astronot_on_health_changed(cur_health) -> void:
+	health_ui.text = "Health: " + str(cur_health)
+
+
+func _on_astronot_on_oxygen_changed(cur_oxygen) -> void:
+	oxygen_ui.text = "Oxygen: " + str(cur_oxygen)
---
+++ scenes/ui/game-ui/gameover-ui/GameoverUI.gd
@@ -0,0 +1,9 @@
+extends Control
+
+
+func _on_restart_button_pressed() -> void:
+	print("Reset")
+
+
+func _on_exit_button_pressed() -> void:
+	print("EXitt")
