28fbc932f9446e37ab1dfedd632edf08a87a1b67
AgilMonkey <muhammadagilfadilah@gmail.com>
2023-09-15 20:36:37 +0800

    Solved conflict

--- NoOxygenGame-ItechGamejam2023/scenes/ui/OxygenUI.gd
+++
@@ -1,5 +0,0 @@
-extends Label
-
-
-func _on_astronot_on_oxygen_changed(cur_oxygen) -> void:
-	text = "Oxygen: " + str(cur_oxygen)
--- NoOxygenGame-ItechGamejam2023/scenes/ui/health_ui.gd
+++
@@ -1,5 +0,0 @@
-extends Label
-
-
-func _on_astronot_on_health_changed(cur_health) -> void:
-	text = "Health: " + str(cur_health)
