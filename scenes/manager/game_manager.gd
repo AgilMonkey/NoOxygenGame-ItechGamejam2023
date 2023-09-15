099dcfda49dc3786558a210a1faa801d7265e189
AgilMonkey <muhammadagilfadilah@gmail.com>
2023-09-15 20:07:42 +0800

    Added highscore and score ui now appear in death

--- scenes/manager/game_manager.gd
+++ scenes/manager/game_manager.gd
@@ -7,3 +7,9 @@ signal on_score_changed(cur_score)
 func _on_score_timer_timeout() -> void:
 	score += 1
 	on_score_changed.emit(score)
+
+
+func on_gameover() -> void:
+	if score > GlobalManager.highscore:
+		GlobalManager.highscore = score
+	set_process(PROCESS_MODE_DISABLED)
--- scenes/ui/game-ui/gameover-ui/GameoverUI.gd
+++ scenes/ui/game-ui/gameover-ui/GameoverUI.gd
@@ -1,6 +1,10 @@
 extends Control
 
 
+@onready var score_ui := $Score
+var cur_score := 0
+
+
 func _on_restart_button_pressed() -> void:
 	get_tree().reload_current_scene()
 
@@ -11,3 +15,8 @@ func _on_exit_button_pressed() -> void:
 
 func _on_astronot_on_death() -> void:
 	visible = true
+	score_ui.text = "SCORE\n" + str(cur_score)+ "\nHIGHSCORE\n" + str(GlobalManager.highscore)
+
+
+func _on_game_manager_on_score_changed(_score) -> void:
+	cur_score = _score
