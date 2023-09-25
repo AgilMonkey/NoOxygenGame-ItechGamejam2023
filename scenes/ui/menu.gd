84c9bd4473b385c714d0a33a7112cf213bb7af1f
AgilMonkey <muhammadagilfadilah@gmail.com>
2023-09-25 23:36:26 +0800

    Menconnect dengan button ke extra level

--- scenes/ui/menu.gd
+++ scenes/ui/menu.gd
@@ -1,6 +1,7 @@
 extends CanvasLayer
 
 var main_scene : PackedScene = load("res://scenes/Main.tscn")
+var extra_scene : PackedScene = load("res://scenes/world/extra_level.tscn")
 
 func _on_start_pressed():
 	get_tree().change_scene_to_packed(main_scene)
@@ -8,3 +9,7 @@ func _on_start_pressed():
 
 func _on_exit_pressed():
 	get_tree().quit()
+
+
+func _on_extra_level_button_pressed() -> void:
+	get_tree().change_scene_to_packed(extra_scene)
